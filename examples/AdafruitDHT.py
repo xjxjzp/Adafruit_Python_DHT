8a9de83b62ee446bcd9c24681d04810b82a279c4
xjxjzp <55169997+xjxjzp@users.noreply.github.com>
2019-12-28 16:33:42 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -39,8 +39,8 @@ a1=1
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
   humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-while a1 <= 10:
-# Un-comment the line below to convert the temperature to Fahrenheit.
+
+:# Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
 
 # Note that sometimes you won't get a reading and
@@ -49,8 +49,10 @@ while a1 <= 10:
 # If this happens try again!
 
   if humidity is not None and temperature is not None:
+    while a1 <= 10 :
       print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+      a1 = a1+1
   else:
       print('Failed to get reading. Try again!')
      sys.exit(1)
-     a1 = a1+1
+    
