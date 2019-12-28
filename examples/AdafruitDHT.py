16b99f0669dad254420aae0b6068de5597b84c86
xjxjzp <55169997+xjxjzp@users.noreply.github.com>
2019-12-28 16:41:54 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -51,10 +51,6 @@ import Adafruit_DHT
   if humidity is not None and temperature is not None:
    
       print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
   else:
       print('Failed to get reading. Try again!')
      sys.exit(1)
