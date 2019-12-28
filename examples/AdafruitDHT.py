976e9b5e3182cabb85ffe76ef88ac7074f7baa4d
xjxjzp <55169997+xjxjzp@users.noreply.github.com>
2019-12-28 16:25:00 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -23,7 +23,7 @@ import sys
 
 import Adafruit_DHT
 a1=1
-while a<=10:
+
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
@@ -47,6 +47,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
+while a<=10:
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
